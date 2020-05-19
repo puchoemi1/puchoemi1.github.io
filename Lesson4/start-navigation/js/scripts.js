445bb785c399781a1f71d0a685965f7a6025b3d9
puchoemi1 <puc16001@byui.edu>
2020-05-19 18:42:28 -0300

    pushing js

--- Lesson4/start-navigation/js/scripts.js
+++ Lesson4/start-navigation/js/scripts.js
@@ -3,33 +3,3 @@ function toggleMenu() {
 
 }
 
-const daynames = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday"
-    ];
-    const months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December"
-    ];
-    const d = new Date();
-    const dayName = daynames[d.getDay()];
-    const monthName = months[d.getMonth()];
-    const year = d.getFullYear();
-    const fulldate = dayName + ", " + d.getDate() + " " + monthName + " " + year;
-    document.getElementById("showdate").textContent = fulldate;
-
